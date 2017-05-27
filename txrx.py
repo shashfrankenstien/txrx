8822a173be1d044f825a4996cf46fa8ac31911eb
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-05-27 12:44:40 -0400

    long msg consistancy

--- txrx.py
+++ txrx.py
@@ -332,7 +332,7 @@ if __name__ == '__main__':
 	if debug > 3: debug=3
 	print 'debug =', debug
 
-
+	start = time.time()
 	RF = RFMessenger(tx_pin=tx, rx_pin=rx, debug=debug)
 
 	def demo_printer(msg):
@@ -345,6 +345,7 @@ if __name__ == '__main__':
 	RF.listen()
 	if RF.ping(RF.__id__, n=3, silent=False):
 		RF.send('3way h-shake is an extremely long message. Seems like it will shit out.')
+	print '3way completed in {}'.format(time.time()-start)
 	time.sleep(4)
 	RF.terminate()
 
