d0b93c45b84c7718dec8bd8969483306f80d2ddc
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-05-27 12:40:56 -0400

    long msg consistancy

--- txrx.py
+++ txrx.py
@@ -338,13 +338,14 @@ if __name__ == '__main__':
 	def demo_printer(msg):
 		print 'Received -> '+str(msg)
 		if '3way' in msg:
+			print 'replying'
 			RF.send('Received handshake')
 
 	RF.subscribe(demo_printer)
 	RF.listen()
 	if RF.ping(RF.__id__, n=3, silent=False):
 		RF.send('3way h-shake is an extremely long message. Seems like it will shit out.')
-	time.sleep(3)
+	time.sleep(4)
 	RF.terminate()
 
 	# RFd = RFDriver(tx_pin=tx, rx_pin=rx, debug=1)
