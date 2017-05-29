ae9cc8f24ea3c8994bc2f02454b67a36ce192ec2
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-05-29 16:03:19 -0400

    test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from txrx import RFMessenger
 import time
 import argparse
+import threading
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-t','--tx', help='Transmitter pin', type=int)
@@ -13,13 +14,23 @@ rx = args.rx
 def defFunc(m):
 	print m
 
+def ping():
+	RF.ping(RF.__id__, silent=False)
+
+def command():
+	while True:
+		c = raw_input()
+		if c:
+			if c=='p':
+				ping()
+			elif c=='q':
+				break
+		print 'unrecognized'
 
 RF = RFMessenger(tx_pin=tx, rx_pin=rx, debug=2)
 RF.subscribe(defFunc)
 RF.listen()
-time.sleep(1)
-RF.ping(RF.__id__, silent=False)
 
+command()
 
-time.sleep(20)
 RF.terminate()
