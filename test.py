098f0fe54c352c8b7b9ba1a3a53cd97d716f4034
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-05-29 04:57:04 -0400

    test.py

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
 from txrx import RFMessenger
-
+import time
 
 def defFunc(m):
 	print m
 
-tx = 35
-rx = 31
+tx = 37
+rx = 40
 
 RF = RFMessenger(tx_pin=tx, rx_pin=rx, debug=2)
 RF.subscribe(defFunc)
 RF.listen()
+time.sleep(1)
 RF.ping(RF.__id__, silent=False)
 
-import time
+
 time.sleep(20)
 RF.terminate()
