a1c3201868b7f928c16a24c976b7dae7c663bfb7
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-05-29 05:05:22 -0400

    test.py

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 from txrx import RFMessenger
 import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t','--tx' help='Transmitter pin')
+parser.add_argument('-r','--rx' help='Receiver pin')
+
+args = parser.parse_args()
+tx = args.tx
+rx = args.rx
 
 def defFunc(m):
 	print m
 
-tx = 37
-rx = 40
 
 RF = RFMessenger(tx_pin=tx, rx_pin=rx, debug=2)
 RF.subscribe(defFunc)
