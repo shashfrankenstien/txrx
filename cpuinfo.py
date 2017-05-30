8028d8d77355b72c1db967ebdfcb4a07542cfb27
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-05-30 18:30:06 -0400

    cpuinfo

--- cpuinfo.py
+++ cpuinfo.py
@@ -1,32 +1,35 @@
-class CPUInfo(dict):
-	__getattr__ = dict.get
-	__setattr__ = dict.__setitem__
-	__delattr__ = dict.__delitem__
+class dotdict(dict):
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
-	with open('/proc/cpuinfo', 'r') as cpuinfo:
-		for i in cpuinfo:
-			try:
-				k, v = i.strip().split(':')
-				self[str(k).strip()] = str(v).strip()
-			except:
-				pass
 
-	def this_is_a_pi(self):
-		if 'BCM' in self.Hardware:
-			return True
-		else: return False
+CPUInfo = dotdict()
 
-	def this_is_a_chip(self):
-		if 'Allwinner' in self.Hardware:
-			return True
-		else: return False
+with open('/proc/cpuinfo', 'r') as cpuinfo:
+	for i in cpuinfo:
+		try:
+			k, v = i.strip().split(':')
+			CPUInfo[str(k).strip()] = str(v).strip()
+		except:
+			pass
 
-	def who_am_i(self):
-		if self.this_is_a_chip():
-			return 'NTC CHIP'
-		if self.this_is_a_pi():
-			return 'RASPBERRY PI'
+def this_is_a_pi():
+	if 'BCM' in CPUInfo.Hardware:
+		return True
+	else: return False
+
+def this_is_a_chip():
+	if 'Allwinner' in CPUInfo.Hardware:
+		return True
+	else: return False
+
+def who_am_i():
+	if this_is_a_chip():
+		return 'NTC CHIP'
+	if this_is_a_pi():
+		return 'RASPBERRY PI'
 
 
 if __name__ == '__main__':
-	print CPUInfo.who_am_i()
+	print who_am_i()
