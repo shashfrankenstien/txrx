238c72655ceb96de376d36ce24ea9c9530461c8b
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-05-30 19:11:36 -0400

    cpuinfo

--- txrx.py
+++ txrx.py
@@ -35,7 +35,7 @@ class TXRXProtocol(object):
 	# short_delay = 0.001
 	# half_pulse = short_delay*0.3
 	short_delay = 0.0004
-	half_pulse = short_delay*0.28
+	half_pulse = short_delay*0.29
 	# short_delay = 0.0006
 	long_delay = short_delay*2
 	# half_pulse = short_delay*0.27
