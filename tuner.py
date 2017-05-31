474e21764c45f3c0956090de2b09bf16c0d4ba92
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-05-31 05:48:56 -0400

    chip test

--- tuner.py
+++ tuner.py
@@ -6,7 +6,15 @@ class RFTuner(RFMessenger):
 
 	def __init__(self, tx_pin, rx_pin, debug=0):
 		super(RFTuner, self).__init__(tx_pin, rx_pin, debug)
+		gpio.add_event_detect(rx_pin, gpio.FALLING, callback=self.listen_callback)
 
 	
-	def listen_callback(self):
-		pass
+	def listen_rising(self):
+		print 'rose'
+
+	def listen_falling(self):
+		print 'fell'
+
+
+if __name__ == '__main__':
+	RF = RFTuner(tx_pin=37, rx_pin=40)
