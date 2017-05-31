5640408555911fa34af1cca49881a60bcfcd743a
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-05-31 03:02:17 -0400

    chip test

--- tuner.py
+++ tuner.py
@@ -1,10 +1,12 @@
-from txrx import RFMessenger
+from txrx import RFMessenger, gpio
 import threading
+
 
 class RFTuner(RFMessenger):
 
 	def __init__(self, tx_pin, rx_pin, debug=0):
 		super(RFTuner, self).__init__(tx_pin, rx_pin, debug)
+
 	
 	def listen_callback(self):
 		pass
