7edc655b29930dbad618ae235bb06bcf88cfb505
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-05-31 04:05:09 -0400

    chip test

--- txrx.py
+++ txrx.py
@@ -43,7 +43,7 @@ class TXRXProtocol(object):
 	# short_delay = 0.0004-_serr
 	# half_pulse = short_delay*0.222
 	short_delay = 0.0008-_serr
-	half_pulse = short_delay*0.214
+	half_pulse = short_delay*0.273
 	long_delay = short_delay*2-_serr
 	stabilizer_byte = '0000'
 	pad_byte = '10011111'
